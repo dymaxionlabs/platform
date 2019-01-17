9dbf3dbb481c57d81aefac202e273a6d9cde0096
Damián Silvani <munshkr@gmail.com>
2019-01-17 18:13:05 -0300

    Leave only JSONRenderer

--- terra/settings.py
+++ terra/settings.py
@@ -153,6 +153,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
