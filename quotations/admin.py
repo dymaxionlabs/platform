15d37883913f8513d734d605d78256594c0af81c
Damián Silvani <munshkr@gmail.com>
2019-01-20 16:02:57 -0300

    Some changes to the model, register in admin

--- quotations/admin.py
+++ quotations/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
 from .models import Quotation
 
-admin.site.register(Quotation)
+admin.site.register(Quotation, admin.OSMGeoAdmin)
--- quotations/models.py
+++ quotations/models.py
@@ -4,12 +4,13 @@ from django.contrib.gis.db import models
 
 
 class Quotation(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     message = models.TextField(null=True, blank=True)
     areas_geom = models.MultiPolygonField(null=True, blank=True)
-    layers = ArrayField(models.CharField(max_length=30))
-    extra_fields = JSONField()
+    layers = ArrayField(models.CharField(max_length=30), null=True, blank=True)
+    extra_fields = JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
