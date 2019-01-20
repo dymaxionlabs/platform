734cea3d2e85139f8ae6296759c3d18c150450e1
Damián Silvani <munshkr@gmail.com>
2019-01-20 12:26:45 -0300

    Configure drf-yasg endpoints

--- terra/settings.py
+++ terra/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'rest_auth',
     'corsheaders',
     'anymail',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- terra/urls.py
+++ terra/urls.py
@@ -16,15 +16,39 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include
-from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
 
 from projects.views import ExampleView
 
 router = routers.DefaultRouter()
 #router.register(r'users', UserViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Terra API",
+        default_version='v1',
+        description="This is the description of the API",
+        terms_of_service="https://www.dymaxionlabs.com/terms/",
+        contact=openapi.Contact(email="contact@dymaxionlabs.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny, ),
+)
+
 urlpatterns = [
     url(r'^example/?', ExampleView.as_view()),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'),
+    url(r'^swagger/$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
     url(r'^auth/', include('rest_auth.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
