d8d771b0109c546fdf6a0c29fd6ba738cb6912c9
Damián Silvani <munshkr@gmail.com>
2019-01-20 16:13:45 -0300

    Default default settings on GeoAdmin

--- quotations/admin.py
+++ quotations/admin.py
@@ -2,4 +2,16 @@ from django.contrib.gis import admin
 
 from .models import Quotation
 
-admin.site.register(Quotation, admin.OSMGeoAdmin)
+
+class QuotationAdmin(admin.OSMGeoAdmin):
+    list_display = ('id', 'name', 'email', 'message', 'created_at')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'message', 'email')
+    list_per_page = 25
+    default_lon = -58.381557
+    default_lat = -34.603683
+    default_zoom = 17
+    modifiable = False
+
+
+admin.site.register(Quotation, QuotationAdmin)
