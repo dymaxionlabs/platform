798a56b079f059cc239dde042e5b2c6f0e4d2298
Damián Silvani <munshkr@gmail.com>
2019-02-21 10:51:06 -0300

    Refactor Layer, LayerImage models

--- projects/admin.py
+++ projects/admin.py
@@ -1,28 +1,30 @@
 from django.contrib import admin
 
-from .models import UserProfile, Client, Project, Layer, LayerImage
+from .models import UserProfile, Client, Project, LayerCollection, Layer
 
 
 class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'description', 'slug')
     prepopulated_fields = {"slug": ("name", )}
 
 
-class LayerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'project', 'slug',
-                    'created_at')
+class LayerCollectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'layer_type', 'project', 'slug',
+                    'description', 'created_at')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'description', 'slug')
     prepopulated_fields = {"slug": ("name", )}
 
 
-class LayerImageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'layer', 'date', 'created_at')
-    list_display_links = ('id', 'layer')
-    search_fields = ('layer', 'date')
+class LayerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'layer_collection', 'date', 'created_at',
+                    'tiles_url')
+    list_display_links = ('id', 'layer_collection', 'date')
+    search_fields = ('layer_collection', 'date')
 
 
 admin.site.register(UserProfile)
 admin.site.register(Client)
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(LayerCollection, LayerCollectionAdmin)
 admin.site.register(Layer, LayerAdmin)
-admin.site.register(LayerImage, LayerImageAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group, User
+from django.contrib.gis.db import models
+from django.contrib.postgres.fields import JSONField
 
 
 class UserProfile(models.Model):
@@ -30,10 +31,20 @@ class Project(models.Model):
         return self.name
 
 
-class Layer(models.Model):
+class LayerCollection(models.Model):
+    RASTER = 'R'
+    VECTOR = 'V'
+    LAYER_CHOICES = (
+        (RASTER, 'Raster'),
+        (VECTOR, 'Vector'),
+    )
+    BASE_TILE_URL = 'https://storage.googleapis.com/dym-tiles/{project_slug}/{slug}'
+
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True, blank=True)
     project = models.ForeignKey(Project, null=True, on_delete=models.SET_NULL)
+    layer_type = models.CharField(
+        max_length=1, choices=LAYER_CHOICES, default=RASTER)
     slug = models.SlugField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -41,12 +52,35 @@ class Layer(models.Model):
     def __str__(self):
         return self.name
 
+    def base_tiles_url(self):
+        return self.BASE_TILE_URL.format(
+            project_slug=self.project.slug, slug=self.slug)
 
-class LayerImage(models.Model):
-    layer = models.ForeignKey(Layer, null=True, on_delete=models.SET_NULL)
+    def tiles_extension(self):
+        if self.layer_type == self.RASTER:
+            return 'png'
+        elif self.layer_type == self.VECTOR:
+            return 'pbf'
+        else:
+            raise TypeError('unknown layer type {}'.format(self.layer_type))
+
+
+class Layer(models.Model):
+    layer_collection = models.ForeignKey(
+        LayerCollection, null=True, on_delete=models.SET_NULL)
+    area_geom = models.PolygonField()
     date = models.DateField()
+    extra_fields = JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.date)
+
+    def tiles_url(self):
+        collection = self.layer_collection
+        base_url = collection.base_tiles_url()
+        date_str = self.date.strftime('%Y-%m-%d')
+        ext = collection.tiles_extension()
+        return '{base_url}/{date}'.format(
+            base_url=base_url, date=date_str) + '/{z}/{x}/{y}.' + ext
