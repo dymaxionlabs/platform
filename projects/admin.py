b980579f1a226f07e11859ebd7e18d8d64d206b7
Damián Silvani <munshkr@gmail.com>
2019-02-24 12:28:55 -0300

    Allow GeoJSON file upload when creating Layer

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.gis.geos import GEOSGeometry
+import json
 
 from .models import Layer, Map, MapLayer, Project, UserProfile
 
@@ -12,7 +15,33 @@ class ProjectAdmin(admin.ModelAdmin):
         return ", ".join([g.name for g in obj.groups.all()])
 
 
+class LayerForm(forms.ModelForm):
+    area_geojson_file = forms.FileField()
+
+    def __init__(self, *args, **kwargs):
+        super(LayerForm, self).__init__(*args, **kwargs)
+        self.fields['area_geom'].required = False
+        self.fields['area_geojson_file'].required = False
+
+    def save(self, commit=True):
+        instance = super(LayerForm, self).save(commit=False)
+        area_geojson_file = self.cleaned_data.get('area_geojson_file', None)
+        if area_geojson_file:
+            geojson = json.load(area_geojson_file)
+            feature = geojson['features'][0]
+            polygon = GEOSGeometry(json.dumps(geojson['features'][0]['geometry']))
+            instance.area_geom = polygon
+        if commit:
+            instance.save()
+        return instance
+
+    class Meta:
+        model = Layer
+        fields = '__all__'
+
+
 class LayerAdmin(admin.ModelAdmin):
+    form = LayerForm
     list_display = ('id', 'name', 'layer_type', 'project', 'date',
                     'created_at', 'tiles_url')
     list_display_links = ('id', 'name')
