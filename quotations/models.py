2a8b19a45e24a29b2b1db166c1eb92862e915475
Damián Silvani <munshkr@gmail.com>
2019-03-28 14:51:46 -0300

    Create RequestStateUpdate model

--- quotations/models.py
+++ quotations/models.py
@@ -33,3 +33,31 @@ class RequestArea(models.Model):
 
     def __str__(self):
         return "Area of {} km²".format(round(self.area_km2()))
+
+
+class RequestStateUpdate(models.Model):
+    PENDING_STATE = 'PENDING'
+    EVALUATING_STATE = 'EVALUATING'
+    AWAITING_PAYMENT_STATE = 'AWAITING_PAYMENT'
+    PROCESSING_STATE = 'PROCESSING'
+    FINISHED_STATE = 'FINISHED'
+    CANCELED_STATE = 'CANCELED'
+
+    STATE_CHOICES = (
+        (PENDING_STATE, _('pending')),
+        (EVALUATING_STATE, _('evaluating')),
+        (AWAITING_PAYMENT_STATE, _('awaiting payment')),
+        (PROCESSING_STATE, _('processing')),
+        (FINISHED_STATE, _('finished')),
+        (CANCELED_STATE, _('canceled')),
+    )
+
+    request = models.ForeignKey(
+        Request, on_delete=models.CASCADE, related_name='state_updates')
+    state = models.CharField(
+        max_length=24, choices=STATE_CHOICES, default=PENDING_STATE)
+    description = models.TextField(null=True, blank=True)
+    extra_fields = JSONField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
