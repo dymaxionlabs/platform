7bd5e5d358e4baa3583c594c5b7b76ceb8d26c5c
Damián Silvani <munshkr@gmail.com>
2019-04-22 10:38:40 -0300

    Create basic Estimator model

--- estimators/models.py
+++ estimators/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
+from django.contrib.postgres.fields import JSONField
+from django.utils.translation import gettext as _
+
+from projects.models import Project
+
+
+class Estimator(models.Model):
+    OBJECT_DETECTION = 'OD'
+    CLASSIFICATION = 'C'
+    TYPE_CHOICES = ((OBJECT_DETECTION, _('Object detection')),
+                    # (CLASSIFICATION, _('Classification')),
+                    )
+
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    project = models.ForeignKey(
+        Project,
+        null=True,
+        on_delete=models.CASCADE,
+        verbose_name=_('project'))
+
+    estimator_type = models.CharField(
+        _('estimator type'),
+        max_length=2,
+        choices=TYPE_CHOICES,
+        default=OBJECT_DETECTION)
+    name = models.CharField(_('name'), max_length=255)
+    metadata = JSONField(null=True, blank=True)
+
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+
+    class Meta:
+        unique_together = (('project', 'estimator_type', 'name'), )
+
+    def __str__(self):
+        return '{name} ({type})'.format(
+            name=self.name, type=self.estimator_type)
