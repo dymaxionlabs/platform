6ba6ad2afe295088ea9db117a0c738f114a28bf4
Damián Silvani <munshkr@gmail.com>
2019-04-23 12:22:47 -0300

    Register ImageTile and Annotation on admin

--- estimators/admin.py
+++ estimators/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Estimator
+from .models import Estimator, ImageTile, Annotation
 
 admin.site.register(Estimator)
+admin.site.register(ImageTile)
+admin.site.register(Annotation)
