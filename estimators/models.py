a40e3f49cc618c3cd553cf030e37a23a24e09a7a
Damián Silvani <munshkr@gmail.com>
2019-04-25 09:05:25 -0300

    Add Window columns (col_off, row_off, width, height) to ImageTile

--- estimators/models.py
+++ estimators/models.py
@@ -1,11 +1,12 @@
 import uuid
 
+import rasterio
 from django.contrib.auth.models import User
 from django.contrib.gis.db import models
 from django.contrib.postgres.fields import ArrayField, JSONField
 from django.utils.translation import gettext as _
 
-from projects.models import Project, File
+from projects.models import File, Project
 
 
 class Estimator(models.Model):
@@ -16,17 +17,15 @@ class Estimator(models.Model):
                     )
 
     uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
-    project = models.ForeignKey(
-        Project,
-        null=True,
-        on_delete=models.CASCADE,
-        verbose_name=_('project'))
+    project = models.ForeignKey(Project,
+                                null=True,
+                                on_delete=models.CASCADE,
+                                verbose_name=_('project'))
 
-    estimator_type = models.CharField(
-        _('estimator type'),
-        max_length=2,
-        choices=TYPE_CHOICES,
-        default=OBJECT_DETECTION)
+    estimator_type = models.CharField(_('estimator type'),
+                                      max_length=2,
+                                      choices=TYPE_CHOICES,
+                                      default=OBJECT_DETECTION)
     name = models.CharField(_('name'), max_length=255)
     classes = ArrayField(models.CharField(max_length=32), default=list)
     metadata = JSONField(null=True, blank=True)
@@ -39,28 +38,43 @@ class Estimator(models.Model):
         unique_together = (('project', 'estimator_type', 'name'), )
 
     def __str__(self):
-        return '{name} ({type})'.format(
-            name=self.name, type=self.estimator_type)
+        return '{name} ({type})'.format(name=self.name,
+                                        type=self.estimator_type)
 
 
 class ImageTile(models.Model):
-    file = models.ForeignKey(
-        File, on_delete=models.CASCADE, verbose_name=_('file'))
-    x = models.IntegerField(default=0)
-    y = models.IntegerField(default=0)
+    file = models.ForeignKey(File,
+                             on_delete=models.CASCADE,
+                             verbose_name=_('file'))
+    col_off = models.IntegerField(default=0)
+    row_off = models.IntegerField(default=0)
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
 
     class Meta:
-        unique_together = (('file', 'x', 'y'))
+        unique_together = (('file', 'col_off', 'row_off', 'width', 'height'))
 
     def __str__(self):
-        return '{file} ({x}, {y})'.format(file=self.file, x=self.x, y=self.y)
+        return '{file} ({col_off}, {row_off}, {width}, {height})'.format(
+            file=self.file,
+            col_off=self.col_off,
+            row_off=self.row_off,
+            width=self.width,
+            height=self.height)
+
+    def window(self):
+        """Returns the Window instance of itself"""
+        return rasterio.windows.Window(self.col_off, self.row_off, self.width,
+                                       self.height)
 
 
 class Annotation(models.Model):
-    estimator = models.ForeignKey(
-        Estimator, on_delete=models.CASCADE, verbose_name=_('estimator'))
-    image_tile = models.OneToOneField(
-        ImageTile, on_delete=models.CASCADE, verbose_name=_('image tile'))
+    estimator = models.ForeignKey(Estimator,
+                                  on_delete=models.CASCADE,
+                                  verbose_name=_('estimator'))
+    image_tile = models.OneToOneField(ImageTile,
+                                      on_delete=models.CASCADE,
+                                      verbose_name=_('image tile'))
     segments = JSONField(null=True, blank=True, verbose_name=_('segments'))
 
     def __str__(self):
