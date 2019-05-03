7f4570e93d7bd57ac12d437b468b9a403535aa17
Damián Silvani <munshkr@gmail.com>
2019-05-03 16:34:03 -0300

    Use Typography on filename preview in PreviewList of DropzoneArea

--- components/upload/PreviewList.js
+++ components/upload/PreviewList.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { isImage } from "./helpers.js";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import DeleteIcon from "@material-ui/icons/Delete";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import Fab from "@material-ui/core/Fab";
@@ -61,7 +62,11 @@ function PreviewList(props) {
           <Grid item xs={4} key={i} className={classes.imageContainer}>
             {img}
 
-            {showFileNames && <p>{fileObject.file.name}</p>}
+            {showFileNames && (
+              <Typography className={classes.filename}>
+                {fileObject.file.name}
+              </Typography>
+            )}
 
             <Fab
               onClick={handleRemove(i)}
