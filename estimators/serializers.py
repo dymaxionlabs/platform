c5aa04222b71f78bc4b86bedb7ecd987eba50061
Damián Silvani <munshkr@gmail.com>
2019-05-06 12:17:50 -0300

    Fix EstimatorSerializer

--- estimators/serializers.py
+++ estimators/serializers.py
@@ -1,22 +1,27 @@
 from rest_framework import serializers
 
-from projects.models import Project
+from projects.models import Project, File
+from projects.mixins import allowed_projects_for
 
 from .models import Estimator
 
 
-class SlugRelatedUserField(serializers.SlugRelatedField):
+class ProjectSlugField(serializers.SlugRelatedField):
     def get_queryset(self):
         user = self.context['request'].user
-        if user.is_staff:
-            queryset = Project.objects.all()
-        else:
-            queryset = user.projects.all()
+        return allowed_projects_for(Project.objects, user)
+
+
+class FileSlugField(serializers.SlugRelatedField):
+    def get_queryset(self):
+        user = self.context['request'].user
+        queryset = File.objects.filter(owner=user)
         return queryset
 
 
 class EstimatorSerializer(serializers.ModelSerializer):
-    project = SlugRelatedUserField(slug_field='uuid')
+    project = ProjectSlugField(slug_field='uuid')
+    image_files = FileSlugField(many=True, slug_field='name')
 
     class Meta:
         model = Estimator
