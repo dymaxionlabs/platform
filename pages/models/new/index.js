d2e22f82d0572025d68497a5e4f99543faad17de
Damián Silvani <munshkr@gmail.com>
2019-05-27 15:33:01 -0300

    Add missing import

--- pages/models/new/index.js
+++ pages/models/new/index.js
@@ -2,6 +2,7 @@ import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
+import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import Head from "next/head";
