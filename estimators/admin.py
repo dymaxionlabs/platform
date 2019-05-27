cd2d6729bdb5e4106987669ebd1af14f33a5c71f
Damián Silvani <munshkr@gmail.com>
2019-05-27 17:19:45 -0300

    Show uuid field on Estimator admin

--- estimators/admin.py
+++ estimators/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Estimator, ImageTile, Annotation
 
-admin.site.register(Estimator)
+
+class EstimatorAdmin(admin.ModelAdmin):
+    list_display = ('project', 'name', 'estimator_type', 'classes', 'uuid')
+
+
+admin.site.register(Estimator, EstimatorAdmin)
 admin.site.register(ImageTile)
 admin.site.register(Annotation)
