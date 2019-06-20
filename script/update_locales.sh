f7d325b17d17bc7373cffbd18a7afc79a1c62c33
Damián Silvani <munshkr@gmail.com>
2019-06-20 11:19:29 -0300

    Update i18n document url

--- script/update_locales.sh
+++ script/update_locales.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-I18N_ID=1AmrxI25tSvKnkbbd4uknAvy_--llcFCaYcd7b_YEegk
+I18N_ID=1-gYnVIAIgUpjQO7vlXkkZs2FiM1jUj6sSyYaK6d8L8E
 #wget "https://docs.google.com/spreadsheets/d/${I18N_ID}/export?format=csv&id=${I18N_ID}&gid=0" -O i18n.csv
 wget "https://docs.google.com/spreadsheets/d/${I18N_ID}/export?format=csv" -O i18n.csv
 script/gen_locales.py
