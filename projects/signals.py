e3821fd17c77bf81b59bf6b7a779841790ada43a
Gessica <gessica@dymaxionlabs.com>
2020-01-22 11:47:58 -0300

    Create "Demo" project when a new user is created

--- projects/signals.py
+++ projects/signals.py
@@ -6,13 +6,15 @@ from django.dispatch import receiver
 
 from terra.emails import WelcomeEmail
 
-from .models import File, UserProfile
+from .models import File, UserProfile, Project
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+        project = Project.objects.create(name='Demo')
+        project.owners.set([instance])
 
 
 @receiver(post_save, sender=User)
@@ -28,7 +30,9 @@ def send_welcome_email(sender, instance, created, **kwargs):
 def generate_raster_tiles_from_file(sender, instance, created, **kwargs):
     ext = os.path.splitext(instance.name)[1]
     if created:
-        if ext in ['.jpg','.tif','.jpeg','.png']:
-            django_rq.enqueue('projects.tasks.generate_raster_tiles', instance.pk)
+        if ext in ['.jpg', '.tif', '.jpeg', '.png']:
+            django_rq.enqueue('projects.tasks.generate_raster_tiles',
+                              instance.pk)
         if ext in ['.json', '.geojson']:
-            django_rq.enqueue('projects.tasks.generate_vector_tiles', instance.pk)
+            django_rq.enqueue('projects.tasks.generate_vector_tiles',
+                              instance.pk)
