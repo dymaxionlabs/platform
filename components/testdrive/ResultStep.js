5d638ae3cd219f4792bd456be687ac31ec27bf17
Gessica <gessica@dymaxionlabs.com>
2020-02-27 15:36:44 -0300

    add url map

--- components/testdrive/ResultStep.js
+++ components/testdrive/ResultStep.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import React from "react";
 import { withNamespaces } from "../../i18n";
 import StepContentContainer from "../StepContentContainer";
+import { routerPush } from "../../utils/router";
 
 const styles = theme => ({
   header: {
@@ -46,8 +47,9 @@ class ResultStep extends React.Component {
   }
 
   handleClickResultMap() {
-    routerPush("../MapPool");
+    routerPush("/view/MapPool");
   }
+
   handleClickResultGeoJSON() {
     alert("To do");
   }
