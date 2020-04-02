07961ac06445ee12e2a24d8d6d9b5412ec9e564a
Damián Silvani <munshkr@gmail.com>
2020-04-02 09:06:40 -0300

    /invite now accepts already logged in users and links to /login page

--- pages/invite.js
+++ pages/invite.js
@@ -13,10 +13,12 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Head from "next/head";
 import withStyles from "@material-ui/core/styles/withStyles";
+import cookie from "js-cookie";
 import axios from "axios";
 import { i18n, withNamespaces, Link } from "../i18n";
 import { withAuthSync, login } from "../utils/auth";
 import { buildApiUrl } from "../utils/api";
+import { routerPush } from "../utils/router";
 
 const styles = theme => ({
   main: {
@@ -62,6 +64,9 @@ const styles = theme => ({
   },
   submit: {
     marginTop: theme.spacing.unit * 3
+  },
+  loginPar: {
+    marginTop: theme.spacing.unit * 2
   }
 });
 
@@ -77,6 +82,7 @@ class Invite extends React.Component {
     err_password2_msg: "",
     inviteHasEmail: false,
     fatalError: "",
+    showForm: false,
     loading: true
   };
 
@@ -90,14 +96,20 @@ class Invite extends React.Component {
   constructor(props) {
     super(props);
 
-    const { t } = props;
+    const { t, token } = props;
     const { key } = props.query;
 
     if (!key) {
       this.state = {
+        ...this.state,
         fatalError: t("invite.invalid_key"),
         loading: false
       };
+    } else {
+      // If already logged in, show Confirm button only
+      if (token) {
+        this.state["alreadyLoggedIn"] = true;
+      }
     }
   }
 
@@ -115,19 +127,16 @@ class Invite extends React.Component {
       .then(response => {
         const { data } = response;
         if (data.email && data.confirmed) {
-          this.setState({ fatalError: t("invite.already_confirmed") });
+          this.setState({
+            fatalError: t("invite.already_confirmed")
+          });
         } else {
           this.setState({
             project: data.project,
             email: data.email,
-            inviteHasEmail: !!data.email
+            inviteHasEmail: !!data.email,
+            showForm: true
           });
-        }
-
-        // If already logged in, show Confirm button only
-        const { token } = this.props;
-        if (token) {
-          // TODO
         }
       })
       .catch(err => {
@@ -155,13 +164,12 @@ class Invite extends React.Component {
     this.setState({ password2: e.target.value });
   };
 
-  _confirmInvitationToken(userToken) {
-    const { t } = this.props;
-    const { key, redirect } = this.props.query;
+  async _confirmInvitationToken(userToken) {
+    const { key } = this.props.query;
     console.log(`confirm with ${userToken}`);
 
-    axios
-      .post(
+    try {
+      return await axios.post(
         buildApiUrl(`/projects/invitations/${key}/confirm/`),
         {},
         {
@@ -170,29 +178,42 @@ class Invite extends React.Component {
             Authorization: userToken
           }
         }
-      )
-      .then(() => {})
-      .catch(err => {
-        if (err.response) {
-          console.error(err);
-        }
-      })
-      .then(() => {
-        // Regardless of confirmation, login user
-        this.setState({
-          successMsg: t("invite.success_msg")
-        });
-
-        if (userToken) {
-          login({ token: userToken, redirectTo: redirect });
-        }
-      });
+      );
+    } catch (err) {
+      if (err.response) {
+        console.error(err);
+      }
+    }
   }
 
-  onSubmit = event => {
+  onConfirmSubmit = async event => {
+    event.preventDefault();
+
+    const { t, token } = this.props;
+    const { redirect } = this.props.query;
+    const confirmResponse = await this._confirmInvitationToken(token);
+
+    this.setState({
+      successMsg: t("invite.success_msg"),
+      loading: false
+    });
+
+    // Set current project
+    const { project } = confirmResponse.data;
+    cookie.set("project", project);
+
+    if (redirect) {
+      routerPush(redirect);
+    } else {
+      routerPush("/home");
+    }
+  };
+
+  onSubmit = async event => {
     event.preventDefault();
 
     const { t } = this.props;
+    const { redirect } = this.props.query;
     const { username, email, password1, password2 } = this.state;
 
     const dataSend = {
@@ -213,48 +234,69 @@ class Invite extends React.Component {
       loading: true
     });
 
-    axios
-      .post(buildApiUrl("/auth/registration/"), dataSend, {
-        headers: { "Accept-Language": i18n.language }
-      })
-      .then(response => {
-        const token = response.data.key;
-        this._confirmInvitationToken(token);
-      })
-      .catch(error => {
-        console.error(error);
+    let response;
+    try {
+      response = await axios.post(
+        buildApiUrl("/auth/registration/"),
+        dataSend,
+        { headers: { "Accept-Language": i18n.language } }
+      );
+    } catch (error) {
+      console.error(error);
 
-        // Generic error message
-        let errorMsg = t("invite.error_msg");
+      // Generic error message
+      let errorMsg = t("invite.error_msg");
 
-        // Parse error messages in response
-        if (error.response && error.response.status === 400) {
-          const { data } = error.response;
-          for (let key in data) {
-            this.setState({ [`err_${key}_msg`]: data[key] });
-          }
-          // Update general error message if available
-          if (error.response.non_field_errors) {
-            errorMsg = error.response.non_field_errors;
-          }
+      // Parse error messages in response
+      if (error.response && error.response.status === 400) {
+        const { data } = error.response;
+        for (let key in data) {
+          this.setState({ [`err_${key}_msg`]: data[key] });
         }
+        // Update general error message if available
+        if (error.response.non_field_errors) {
+          errorMsg = error.response.non_field_errors;
+        }
+      }
 
-        this.setState({
-          errorMsg: errorMsg,
-          successMsg: "",
-          loading: false
-        });
-      })
-      .then(() => {
-        this.setState({
-          loading: false
-        });
+      this.setState({
+        errorMsg: errorMsg,
+        successMsg: "",
+        loading: false
       });
+    }
+
+    const token = response.data.key;
+    const confirmResponse = await this._confirmInvitationToken(token);
+
+    // Regardless of confirmation, login user
+    this.setState({
+      successMsg: t("invite.success_msg"),
+      loading: false
+    });
+
+    // Set current project
+    const { project } = confirmResponse.data;
+    cookie.set("project", project);
+
+    login({ token: userToken, redirectTo: redirect });
   };
 
   render() {
     const { t, classes } = this.props;
-    const { loading, email, project, fatalError } = this.state;
+    const { key, redirect } = this.props.query;
+    const {
+      loading,
+      email,
+      project,
+      fatalError,
+      alreadyLoggedIn,
+      showForm
+    } = this.state;
+
+    const loginRedirect = `/invite?key=${encodeURIComponent(
+      key
+    )}&redirect=${encodeURIComponent(redirect || "")}`;
 
     return (
       <main className={classes.main}>
@@ -271,7 +313,9 @@ class Invite extends React.Component {
           <Typography className={classes.subheader}>
             {!loading &&
               !fatalError &&
-              t("invite.subheader", { project, email })}
+              (alreadyLoggedIn
+                ? t("invite.subheader_already_logged_in", { project })
+                : t("invite.subheader", { project, email }))}
           </Typography>
           <Typography className={classes.successMessage}>
             {this.state.successMsg}
@@ -279,95 +323,137 @@ class Invite extends React.Component {
           <Typography className={classes.errorMessage}>
             {this.state.errorMsg || fatalError}
           </Typography>
-          <form className={classes.form} method="post" onSubmit={this.onSubmit}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="username">
-                {t("invite.username_placeholder")}
-              </InputLabel>
-              <Input
-                id="username"
-                name="username"
-                autoComplete="user"
-                autoFocus
-                onChange={this.onUsernameChange}
-                value={this.state.username}
-              />
-              {this.state.err_username_msg && (
-                <FormHelperText error>
-                  {this.state.err_username_msg}
-                </FormHelperText>
-              )}
-            </FormControl>
-            {!this.state.inviteHasEmail && (
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">
-                  {t("invite.email_placeholder")}
-                </InputLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  autoComplete="email"
-                  type="email"
-                  onChange={this.onEmailChange}
-                  value={this.state.email}
-                />
-                {this.state.err_email_msg && (
-                  <FormHelperText error>
-                    {this.state.err_email_msg}
-                  </FormHelperText>
-                )}
-              </FormControl>
-            )}
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password1">
-                {t("invite.password1_placeholder")}
-              </InputLabel>
-              <Input
-                name="password1"
-                type="password"
-                id="password1"
-                onChange={this.onPassword1Change}
-                value={this.state.password1}
-              />
-              {this.state.err_password1_msg && (
-                <FormHelperText error>
-                  {this.state.err_password1_msg}
-                </FormHelperText>
-              )}
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password2">
-                {t("invite.password2_placeholder")}
-              </InputLabel>
-              <Input
-                name="password2"
-                type="password"
-                id="password2"
-                onChange={this.onPassword2Change}
-                value={this.state.password2}
-              />
-              {this.state.err_password2_msg && (
-                <FormHelperText error>
-                  {this.state.err_password2_msg}
-                </FormHelperText>
-              )}
-            </FormControl>
-            <Grid container spacing={24}>
-              <Grid item xs>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  disabled={fatalError !== "" || loading}
-                  className={classes.submit}
+          {showForm &&
+            (alreadyLoggedIn ? (
+              <form
+                className={classes.form}
+                method="post"
+                onSubmit={this.onConfirmSubmit}
+              >
+                <FormControl margin="normal" required fullWidth>
+                  <Grid container spacing={24}>
+                    <Grid item xs>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                        disabled={fatalError !== "" || loading}
+                      >
+                        {t("invite.confirm")}
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </FormControl>
+              </form>
+            ) : (
+              <>
+                <form
+                  className={classes.form}
+                  method="post"
+                  onSubmit={this.onSubmit}
                 >
-                  {t("invite.submit")}
-                </Button>
-              </Grid>
-            </Grid>
-            {loading && <LinearProgress />}
-          </form>
+                  <FormControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="username">
+                      {t("invite.username_placeholder")}
+                    </InputLabel>
+                    <Input
+                      id="username"
+                      name="username"
+                      autoComplete="user"
+                      autoFocus
+                      onChange={this.onUsernameChange}
+                      value={this.state.username}
+                    />
+                    {this.state.err_username_msg && (
+                      <FormHelperText error>
+                        {this.state.err_username_msg}
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                  {!this.state.inviteHasEmail && (
+                    <FormControl margin="normal" required fullWidth>
+                      <InputLabel htmlFor="email">
+                        {t("invite.email_placeholder")}
+                      </InputLabel>
+                      <Input
+                        id="email"
+                        name="email"
+                        autoComplete="email"
+                        type="email"
+                        onChange={this.onEmailChange}
+                        value={this.state.email}
+                      />
+                      {this.state.err_email_msg && (
+                        <FormHelperText error>
+                          {this.state.err_email_msg}
+                        </FormHelperText>
+                      )}
+                    </FormControl>
+                  )}
+                  <FormControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="password1">
+                      {t("invite.password1_placeholder")}
+                    </InputLabel>
+                    <Input
+                      name="password1"
+                      type="password"
+                      id="password1"
+                      onChange={this.onPassword1Change}
+                      value={this.state.password1}
+                    />
+                    {this.state.err_password1_msg && (
+                      <FormHelperText error>
+                        {this.state.err_password1_msg}
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                  <FormControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="password2">
+                      {t("invite.password2_placeholder")}
+                    </InputLabel>
+                    <Input
+                      name="password2"
+                      type="password"
+                      id="password2"
+                      onChange={this.onPassword2Change}
+                      value={this.state.password2}
+                    />
+                    {this.state.err_password2_msg && (
+                      <FormHelperText error>
+                        {this.state.err_password2_msg}
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                  <Grid container spacing={24}>
+                    <Grid item xs>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                        disabled={fatalError !== "" || loading}
+                        className={classes.submit}
+                      >
+                        {t("invite.submit")}
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </form>
+                <Typography className={classes.loginPar}>
+                  {t("invite.already_has_account")}{" "}
+                  <Link
+                    href={{
+                      pathname: "/login",
+                      query: { redirect: loginRedirect }
+                    }}
+                  >
+                    <a>{t("invite.login")}</a>
+                  </Link>
+                </Typography>
+              </>
+            ))}
+          {loading && <LinearProgress />}
         </Paper>
       </main>
     );
