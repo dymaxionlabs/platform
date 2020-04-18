c7278a3161bf58be57a56e89adc5dbfa367792be
Damián Silvani <munshkr@gmail.com>
2020-04-18 20:13:33 -0300

    list_files now allow glob patterns (using fnmatch)

--- storage/client.py
+++ storage/client.py
@@ -1,10 +1,11 @@
 import logging
 import os
+from fnmatch import fnmatch
 
 from django.conf import settings
 from google.cloud import storage as gcs
 
-_logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class Client:
@@ -13,19 +14,22 @@ class Client:
         self._client = None
         self._bucket = None
 
-    def list_files(self, **kwargs):
-        _logger.debug("List blobs on {}".format(self._prefix))
+    def list_files(self, glob_path=""):
+        clean_path = glob_path.lstrip(" /").rstrip()
+        prefix = clean_path.split("*")[0]
+        full_prefix = os.path.join(self._prefix, prefix)
+        logger.info("List blobs on {}".format(full_prefix))
         blobs = self.client.list_blobs(settings.FILES_BUCKET,
-                                       prefix=self._prefix,
-                                       versions=False,
-                                       **kwargs)
-        return (File(blob) for blob in blobs)
+                                       prefix=full_prefix,
+                                       versions=False)
+        files = (File(blob) for blob in blobs)
+        return (f for f in files if fnmatch(f.name, clean_path))
 
     def upload_from_filename(self, filename, to='', content_type=None):
-        full_path = self._get_path(to)
-        _logger.debug("Create blob on {}".format(full_path))
+        full_path = self._get_path(to, filename=filename)
+        logger.info("Create blob on {}".format(full_path))
         blob = self.bucket.blob(full_path)
-        _logger.debug("Upload from filename {} with content type {}".format(
+        logger.info("Upload from filename {} with content type {}".format(
             filename, content_type))
         blob.upload_from_filename(filename=filename, content_type=content_type)
         return File(blob)
@@ -37,9 +41,9 @@ class Client:
                          size=None,
                          content_type=None):
         full_path = self._get_path(to)
-        _logger.debug("Create blob on {}".format(full_path))
+        logger.info("Create blob on {}".format(full_path))
         blob = self.bucket.blob(full_path)
-        _logger.debug(
+        logger.info(
             "Upload from file with: rewind={rewind}, size={size}, content_type={content_type}"
             .format(rewind=rewind, size=size, content_type=content_type))
         blob.upload_from_file(file_obj,
@@ -68,7 +72,9 @@ class Client:
 
     def _get_path(self, to, filename=None):
         res = os.path.join(self._prefix, to.lstrip(" /"))
-        if filename and os.path.basename(res) == '':
+        if os.path.basename(res) == '':
+            if not filename:
+                raise ValueError("{} is a directory")
             name = os.path.basename(filename)
             res = os.path.join(res, name)
         return res
@@ -96,3 +102,10 @@ class File:
     @property
     def name(self):
         return "/".join(self.blob.name.split("/")[1:])
+
+
+def upload_directory(rootdir, *, client):
+    for root, _subdirs, files in os.walk(rootdir):
+        for file in files:
+            path = os.path.join(root, file)
+            client.upload_from_filename(path, to='{}/'.format(root))
