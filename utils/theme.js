23d6a691365dd2578aced230bfa8f1e545dc3d6f
Damián Silvani <munshkr@gmail.com>
2020-05-13 17:00:04 -0300

    Change theme back to previous colors

--- utils/theme.js
+++ utils/theme.js
@@ -1,10 +1,10 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { red, teal, pink } from '@material-ui/core/colors';
+import { red, blue, pink } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
-    primary: teal,
+    primary: blue,
     secondary: pink,
     error: {
       main: red.A400,
