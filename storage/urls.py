6acf4d966ba0a9ec90532c1a97bc8023cafb50d5
Damián Silvani <munshkr@gmail.com>
2020-05-14 11:09:16 -0300

    Define endpoints for resumable uploads (wip)

--- storage/urls.py
+++ storage/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url
 
-from storage.views import ListFile, UploadFile, FileView
+from storage.views import (CreateResumableUpload, FileView, ListFile,
+                           ResumableUpload, UploadFile)
 
 urlpatterns = [
     url(r'^files/?$', ListFile.as_view(), name='index'),
-    url(r'^file/?$', FileView.as_view(), name='retrieve'),
-    url(r'^upload/?$', UploadFile.as_view(), name='upload')
+    url(r'^file/?$', FileView.as_view()),
+    url(r'^upload/?$', UploadFile.as_view(), name='upload'),
+    url(r'^create-resumable-upload/?$',
+        CreateResumableUpload.as_view(),
+        name='create-resumable-upload'),
+    url(r'^resumable-upload/?$',
+        ResumableUpload.as_view(),
+        name='resumable-upload')
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -5,6 +6,7 @@ from rest_framework import status
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.reverse import reverse
 from rest_framework.serializers import ValidationError
 from rest_framework.views import APIView, Response
 
@@ -71,9 +73,13 @@ class UploadFile(StorageAPIView):
 
     manual_parameters = [
         openapi.Parameter('path',
-                          openapi.IN_QUERY,
+                          openapi.IN_FORM,
                           description="File output path in storage",
-                          type=openapi.TYPE_STRING)
+                          type=openapi.TYPE_STRING),
+        openapi.Parameter('file',
+                          openapi.IN_FORM,
+                          description="File content",
+                          type=openapi.TYPE_FILE)
     ]
     responses = {
         200: FileSerializer,
@@ -83,13 +89,18 @@ class UploadFile(StorageAPIView):
     @swagger_auto_schema(manual_parameters=manual_parameters,
                          responses=responses)
     def post(self, request, format=None):
-        client = self.get_client()
         path = request.data.get('path', None)
         if not path:
             raise ParseError("'path' missing")
-        file = client.upload_from_file(request.data['file'],
+        fileobj = request.data.get('file', None)
+        if not fileobj:
+            raise ParseError("'file' missing")
+
+        client = self.get_client()
+        file = client.upload_from_file(fileobj,
                                        to=path,
                                        content_type=request.content_type)
+
         return Response(dict(detail=FileSerializer(file).data),
                         status=status.HTTP_200_OK)
 
@@ -137,3 +148,48 @@ class FileView(StorageAPIView):
         files[0].delete()
         return Response(dict(detail='File deleted.'),
                         status=status.HTTP_200_OK)
+
+
+class CreateResumableUpload(StorageAPIView):
+    manual_parameters = [
+        openapi.Parameter('path',
+                          openapi.IN_QUERY,
+                          description="File path in storage",
+                          type=openapi.TYPE_STRING),
+        openapi.Parameter('size',
+                          openapi.IN_QUERY,
+                          description="Total file size in bytes (optional)",
+                          type=openapi.TYPE_INTEGER),
+    ]
+    responses = {
+        200: openapi.Response("Upload session URL"),
+        400: openapi.Response("Bad request"),
+    }
+
+    @swagger_auto_schema(manual_parameters=manual_parameters,
+                         responses=responses)
+    def post(self, request):
+        path = request.query_params.get('path', None)
+        if not path:
+            raise ParseError("'path' missing")
+        size = request.query_params.get('size', None)
+        if size:
+            size = int(size)
+
+        client = self.get_client()
+        upload_id = client.create_resumable_upload_session(
+            to=path, size=size, content_type=request.content_type)
+
+        url = reverse("resumable-upload", request=request)
+        url = f'{url}?id={upload_id}'
+        return Response(dict(url=url), status=status.HTTP_200_OK)
+
+
+class ResumableUpload(StorageAPIView):
+    def put(self, request):
+        upload_id = request.query_params.get('id', None)
+        if not upload_id:
+            raise ParseError("'id' missing")
+        gcs_url = f''
+        response = requests.put(gcs_url)
+        return Response(response.data, status=response.status_code)
