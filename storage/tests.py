d9e506135e28701444d5ca4461eb5ac806b67c27
Gessica <gessica@dymaxionlabs.com>
2020-05-22 14:03:35 -0300

    add test list file and tearDown

--- storage/tests.py
+++ storage/tests.py
@@ -15,24 +15,59 @@ from terra.tests import create_some_user
 
 
 class ListFilesTest(TestCase):
-    def test_list_file(self):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_some_user()
+        self.project = Project.objects.filter(owners=self.user).first()
+        _, self.api_key = create_some_api_key(user=self.user,
+                                              project=self.project)
+        login_with_api_key(self.client, self.api_key)
+        self.storage_client = Client(self.project)
         with open("/tmp/testfile1.txt", "w") as f:
             f.write("this is a test\n")
         with open("/tmp/testfile2.py", "w") as f:
             f.write("# this is another test\n")
+        self.storage_client.upload_from_filename("/tmp/testfile1.txt")
+        self.storage_client.upload_from_filename("/tmp/testfile2.py", "foo/")
+        self.check_files = ["testfile1.txt", "foo/testfile2.py"]
 
-        self.cl = Client(Project.objects.first())
-        self.cl.upload_from_filename("/tmp/testfile1.txt")
-        self.cl.upload_from_filename("/tmp/testfile2.py", "foo/")
-
+    def test_client_list_file(self):
         uploaded_files = []
-        for f in self.cl.list_files("*testfile*"):
+        for f in self.storage_client.list_files("*testfile*"):
             uploaded_files.append(f.path)
 
-        check_files = ["testfile1.txt", "foo/testfile2.py"]
-
-        for f in check_files:
+        for f in self.check_files:
             self.assertIn(f, uploaded_files)
+
+    def test_list_file(self):
+        response = self.client.get(f'/storage/files/?path=foo*')
+        self.assertEquals(200, response.status_code)
+        file_retrieved = response.data[0]['path']
+        self.assertIn(self.check_files[1], file_retrieved)
+
+    def test_list_no_content(self):
+        response = self.client.get(f'/storage/files/?path=bar')
+        self.assertEquals(204, response.status_code)
+        self.assertFalse(response.data)
+
+    def test_list_no_path(self):
+        response = self.client.get(f'/storage/files/')
+
+        self.assertEquals(200, response.status_code)
+
+        files_retrieved = []
+        for f in response.data:
+            files_retrieved.append(f['path'])
+
+        for f in self.check_files:
+            self.assertIn(f, files_retrieved)
+
+    def tearDown(self):
+        files = list(self.storage_client.list_files())
+        if not files:
+            raise FileNotFoundError
+        for f in files:
+            f.delete()
 
 
 class UploadFileViewTest(TestCase):
