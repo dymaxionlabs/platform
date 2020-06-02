2bf4fa1c02ac27e275282da5952c91eb3dfa566a
alan <alan@dymaxionlabs.com>
2020-06-02 11:55:24 -0300

    reorder urls to avoid issues

--- estimators/urls.py
+++ estimators/urls.py
@@ -10,11 +10,11 @@ router.register(r'annotations', views.AnnotationViewSet)
 router.register(r'image_tiles', views.ImageTileViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^(?P<uuid>[^/]+)/segments_per_label/?',
         views.SegmentsPerLabelView.as_view()),
     url(r'^(?P<uuid>[^/]+)/train/?', views.StartTrainingJobView.as_view()),
     url(r'^(?P<uuid>[^/]+)/predict/?', views.StartPredictionJobView.as_view()),
     url(r'^(?P<uuid>[^/]+)/load_labels/?', views.AnnotationUpload.as_view()),
     url(r'^start_tailing_job/?', views.StartImageTilingJobView.as_view()),
+    url(r'^', include(router.urls)),
 ]
