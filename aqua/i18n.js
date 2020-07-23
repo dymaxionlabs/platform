7f2b7a03f780188dfed47e30df6ec562996cd715
Damián Silvani <munshkr@gmail.com>
2020-07-23 14:09:03 -0300

    Disable browser language detection

--- aqua/i18n.js
+++ aqua/i18n.js
@@ -2,5 +2,6 @@ var NextI18Next = require("next-i18next").default;
 
 module.exports = new NextI18Next({
   defaultLanguage: "en",
-  otherLanguages: ["es"]
+  otherLanguages: ["es"],
+  browserLanguageDetection: false,
 });
