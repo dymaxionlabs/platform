ce2d8e907499b5ff3276e40f1983d0d6741a4012
Damián Silvani <munshkr@gmail.com>
2020-07-24 20:35:39 -0300

    Add signal handler for task_finished to substract credit

--- terra/credits/signals.py
+++ terra/credits/signals.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from estimators.models import Estimator
+from tasks.models import Task
+from tasks.signals import task_finished
+
 from .models import LogEntry
 
 
@@ -12,3 +16,24 @@ def add_initial_credit(sender, instance, created, **kwargs):
                                 kind='initial',
                                 description='Initial credit',
                                 value=10000)
+
+
+@receiver(task_finished, sender=Task)
+def add_training_prediction_entry(sender, task, **kwargs):
+    if task.name in [
+            Estimator.TRAINING_JOB_TASK, Estimator.PREDICTION_JOB_TASK
+    ]:
+        if task.name == Estimator.TRAINING_JOB_TASK:
+            kind = 'training-task'
+            description = 'Training task'
+        elif task.name == Estimator.PREDICTION_JOB_TASK:
+            kind = 'prediction-task'
+            description = 'Prediction task'
+        else:
+            kind = 'task'
+            description = 'Task'
+        cost = round(task.duration * 300)
+        LogEntry.objects.create(user=instance,
+                                kind=kind,
+                                description=description,
+                                value=-cost)
