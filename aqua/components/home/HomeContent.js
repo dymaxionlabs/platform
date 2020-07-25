55a5208072a929a92937744925ec16fbc045dc27
Damián Silvani <munshkr@gmail.com>
2020-07-25 20:21:19 -0300

    Make a grid of cards in HomeContent

--- aqua/components/home/HomeContent.js
+++ aqua/components/home/HomeContent.js
@@ -1,51 +1,98 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-import { withStyles } from "@material-ui/core/styles";
-import FileCopyIcon from "@material-ui/icons/FileCopy";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import Link from "next/link";
-
-import { withTranslation } from "../../i18n";
-import cookie from "js-cookie";
-
 import {
-  IconButton,
-  InputAdornment,
   Button,
   Typography,
-  TextField,
-  Paper,
+  Card,
+  CardContent,
+  CardActions,
+  CardActionArea,
+  Grid,
 } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import cookie from "js-cookie";
+import PropTypes from "prop-types";
+import React from "react";
+import { withTranslation, Link } from "../../i18n";
 
 const styles = (theme) => ({
   root: {
-    width: "100%",
-    overflowX: "auto",
-    padding: "20px",
-  },
-  table: {
-    minWidth: 700,
-  },
-  title: {
-    marginBottom: theme.spacing(2),
-  },
-  paragraph: {
-    marginBottom: theme.spacing(1),
-  },
-  anchorButton: {
-    textDecoration: "none",
-  },
-  section: {
-    marginBottom: theme.spacing(2),
+    flexGrow: 1,
   },
 });
 
-const P = withStyles(styles)(({ children, classes }) => (
-  <Typography variant="body1" component="p" className={classes.paragraph}>
-    {children}
-  </Typography>
-));
+let WelcomeCard = ({ classes }) => (
+  <Card className={classes.cardRoot}>
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="h2">
+        Welcome to Dymaxion Labs Platform
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p">
+        If this is your first time here, we recommend you to do a{" "}
+        <strong>Test Drive</strong> to understand what you can achieve with our{" "}
+        <strong>Models</strong> application.
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Link href="/testdrive">
+        <Button size="small" color="primary">
+          Test Drive
+        </Button>
+      </Link>
+    </CardActions>
+  </Card>
+);
+
+WelcomeCard = withStyles(styles)(WelcomeCard);
+
+let CreditsCard = ({ classes }) => <Card></Card>;
+
+CreditsCard = withStyles(styles)(CreditsCard);
+
+let PythonSDKCard = ({ classes }) => (
+  <Card className={classes.cardRoot}>
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="h2">
+        API and Python SDK
+      </Typography>
+      <Typography
+        gutterBottom
+        variant="body2"
+        color="textSecondary"
+        component="p"
+      >
+        Generate an API Key from the <strong>Keys</strong> section to interact
+        with our API using{" "}
+        <a href="https://docs.dymaxionlabs.com/" target="_blank">
+          Python
+        </a>
+        .
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p">
+        Check out the <strong>Tutorial</strong> in the Python SDK documentation,
+        and this{" "}
+        <a
+          href="https://colab.research.google.com/drive/1LBME8Fn8n1WuWey4I3Icu8K9UWzOgj3i?usp=sharing"
+          target="_blank"
+        >
+          Google Colaboratory notebook
+        </a>
+        .
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <a
+        href="https://docs.dymaxionlabs.com/en/latest/#tutorial"
+        target="_blank"
+        className={classes.anchor}
+      >
+        <Button size="small" color="primary">
+          Tutorial
+        </Button>
+      </a>
+    </CardActions>
+  </Card>
+);
+
+PythonSDKCard = withStyles(styles)(PythonSDKCard);
 
 class HomeContent extends React.Component {
   render() {
@@ -53,61 +100,30 @@ class HomeContent extends React.Component {
     const projectId = cookie.get("project");
 
     return (
-      <div>
-        <Paper className={classes.root}>
-          <Typography variant="h5" component="h2" className={classes.title}>
-            Welcome to Dymaxion Labs Platform
-          </Typography>
-          <div className={classes.section}>
-            <Typography variant="h6" component="h3" className={classes.title}>
-              {t(`home.models_title`)}
-            </Typography>
-            <P>
-              If this is your first time here, we recommend you to do a{" "}
-              <strong>Test Drive</strong> to understand what you can achieve
-              with our <strong>Models</strong> application.
-            </P>
-            {/* <a href="/testdrive" className={classes.anchorButton}> */}
-            <Link href="/testdrive">
-              <Button variant="contained" color="primary">
-                Test Drive
-              </Button>
-            </Link>
-          </div>
-          <div className={classes.section}>
-            <Typography variant="h6" component="h3" className={classes.title}>
-              API and Python SDK
-            </Typography>
-            <P>
-              Generate an API Key from the{" "}
-              <Link href="/home/keys">
-                <a>Keys</a>
-              </Link>{" "}
-              section to interact with our API using{" "}
-              <a href="https://docs.dymaxionlabs.com/" target="_blank">
-                Python
-              </a>
-              .
-            </P>
-            <P>
-              Check out the{" "}
-              <a
-                href="https://docs.dymaxionlabs.com/en/latest/#tutorial"
-                target="_blank"
-              >
-                Tutorial
-              </a>{" "}
-              in the Python SDK documentation, and this{" "}
-              <a
-                href="https://colab.research.google.com/drive/1LBME8Fn8n1WuWey4I3Icu8K9UWzOgj3i?usp=sharing"
-                target="_blank"
-              >
-                Google Colaboratory notebook
-              </a>
-              .
-            </P>
-          </div>
-        </Paper>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <WelcomeCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PythonSDKCard />
+          </Grid>
+          <Grid item xs={6}>
+            <WelcomeCard />
+          </Grid>
+          <Grid item xs={3}>
+            <WelcomeCard />
+          </Grid>
+          <Grid item xs={3}>
+            <WelcomeCard />
+          </Grid>
+          <Grid item xs={3}>
+            <WelcomeCard />
+          </Grid>
+          <Grid item xs={3}>
+            <WelcomeCard />
+          </Grid>
+        </Grid>
       </div>
     );
   }
