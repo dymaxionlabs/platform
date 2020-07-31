3f943d83c1a2e5c855b6dd6a18d69688c176c41b
Damián Silvani <munshkr@gmail.com>
2020-07-31 12:19:47 -0300

    Clean up CloudMLClient

--- terra/tasks/clients.py
+++ terra/tasks/clients.py
@@ -1,9 +1,15 @@
-# GSA needs "ML Engine Developer" permission
 from googleapiclient import discovery
 from googleapiclient import errors
 from django.conf import settings
 
+
 class CloudMLClient:
+    """
+    CloudMLClient allows you to query about jobs, create new training jobs and cancel running jobs
+
+    Note: Service Account will need "ML Engine Developer" permission.
+
+    """
     def __init__(self):
         self.project_id = f'projects/{settings.CLOUDML_PROJECT}'
         self._client = None
@@ -15,14 +21,12 @@ class CloudMLClient:
         return self._client
 
     def list_jobs(self):
-        req = self.client.projects().jobs().list(
-                parent=self.project_id)
+        req = self.client.projects().jobs().list(parent=self.project_id)
         return req.execute()
 
     def get_job(self, job_id):
         name = f'{self.project_id}/jobs/{job_id}'
-        req = self.client.projects().jobs().get(
-                name=name)
+        req = self.client.projects().jobs().get(name=name)
         return req.execute()
 
     def create_job(self, job_id, body):
@@ -32,53 +36,3 @@ class CloudMLClient:
     def cancel_job(self, job_id):
         # TODO
         pass
-
-{
-  "jobId": string,
-  "etag": string,
-  "trainingInput": {
-      "scaleTier": enum (ScaleTier),
-      "masterType": string,
-      "masterConfig": {
-        object (ReplicaConfig)
-      },
-      "workerType": string,
-      "workerConfig": {
-        object (ReplicaConfig)
-      },
-      "parameterServerType": string,
-      "parameterServerConfig": {
-        object (ReplicaConfig)
-      },
-      "evaluatorType": string,
-      "evaluatorConfig": {
-        object (ReplicaConfig)
-      },
-      "workerCount": string,
-      "parameterServerCount": string,
-      "evaluatorCount": string,
-      "packageUris": [
-        string
-      ],
-      "pythonModule": string,
-      "args": [
-        string
-      ],
-      "hyperparameters": {
-        object (HyperparameterSpec)
-      },
-      "region": string,
-      "jobDir": string,
-      "runtimeVersion": string,
-      "pythonVersion": string,
-      "encryptionConfig": {
-        object (EncryptionConfig)
-      },
-      "scheduling": {
-        object (Scheduling)
-      },
-      "network": string,
-      "serviceAccount": string,
-      "useChiefInTfConfig": boolean
-  }
-}
