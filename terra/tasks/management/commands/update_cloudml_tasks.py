49508f4ffa032484c3de3bdde1039923585b092a
Damián Silvani <munshkr@gmail.com>
2020-07-31 12:25:52 -0300

    Management command for updating CloudML-based tasks state

---
+++ terra/tasks/management/commands/update_cloudml_tasks.py
@@ -0,0 +1,50 @@
+from django.core.management.base import BaseCommand
+from django.utils.dateparse import parse_datetime
+
+from tasks.clients import CloudMLClient
+from tasks.models import Task
+from tasks import states
+
+
+class Command(BaseCommand):
+    help = 'Update status of running Cloud ML-based tasks'
+
+    def handle(self, *args, **options):
+        client = CloudMLClient()
+
+        # Look for running Tasks of type TRAIN or PREDICT
+        cloudml_tasks = Task.objects.filter(
+            internal_metadata__uses_cloudml=True,
+            state__in=[states.PENDING, states.STARTED])
+        count = cloudml_tasks.count()
+
+        if count == 0:
+            print("No running tasks. Do nothing")
+            return
+        print("Running tasks:", count)
+
+        # Get all CloudML jobs
+        all_jobs = client.list_jobs()['jobs']
+        print("CloudML jobs:", len(all_jobs))
+
+        jobs_by_task_id = [(job['jobId'].split('_')[1], job)
+                           for job in all_jobs]
+        jobs_by_task_id = {
+            int(id): job
+            for id, job in jobs_by_task_id if id.isnumeric()
+        }
+
+        # For each task, get the corresponding Cloud ML Job
+        # If CloudML job has finished, update state of task
+        for task in cloudml_tasks:
+            # Ref: https://cloud.google.com/ai-platform/training/docs/reference/rest/v1/projects.jobs#State
+            job = jobs_by_task_id[task.pk]
+            state = job['state']
+            # Use real endTime as finished_at, not current time
+            finished_at = parse_datetime(job['endTime']
+            if state == 'SUCCEEDED':
+                task.mark_as_finished(finished_at=finished_at)
+            if state in ['CANCELLING', 'CANCELED']:
+                task.mark_as_canceled(finished_at=finished_at)
+            if state == 'FAILED':
+                task.mark_as_failed(finished_at=finished_at)
