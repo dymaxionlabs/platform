eba632e19acfeefed28b104590346f20d5ec4ef5
Damián Silvani <munshkr@gmail.com>
2020-07-30 22:54:23 -0300

    Update e2e test

--- tests/e2e/e2e_test.py
+++ tests/e2e/e2e_test.py
@@ -16,14 +16,17 @@ def run_object_detection_e2e_test():
         datetime.now().strftime("%Y/%m/%d-%H:%M:%S")),
                                  type="object_detection",
                                  classes=["tree"])
-    print("Estimator created")
-    print(estimator)
+    print("Estimator created:", estimator)
 
     # file upload and tiling
     train_img_path = 'images/orthomosaic.tif'
-    storage_train_img = '{}/image/'.format(storage_root)
-    img = File.upload(train_img_path, storage_train_img)
-    print("File uploaded")
+    storage_train_img_path = '{}/image/orthomosaic.tif'.format(storage_root)
+    img = File.get(storage_train_img_path, raise_error=False)
+    if not img:
+        import pdb
+        pdb.set_trace()
+        img = File.upload(train_img_path, storage_train_img)
+        print("File uploaded")
     print(img)
     estimator.add_image(img)
     print("File added to estimator")
@@ -35,6 +38,8 @@ def run_object_detection_e2e_test():
         time.sleep(60)
 
     print("Tiling finished. Status= {}".format(tiling_job.state))
+    if tiling_job.state == "FAILED":
+        raise "Tiling job failed!"
 
     # annotation upload
     annotations_filename = 'vineyard_annotations.geojson'
@@ -44,25 +49,31 @@ def run_object_detection_e2e_test():
     print("Labels added")
 
     # Training time
-    estimator.configuration['training_hours'] = 0.4
+    estimator.configuration.update(epochs=1, steps=150)
     estimator.save()
 
     print("Start training...")
-    job = estimator.train()
-    print("Task: {}".format(job.id))
-    while job.is_running():
-        time.sleep(60)
+    training_job = estimator.train()
+    print("Task: {}".format(training_job.id))
+    while training_job.is_running():
+        time.sleep(5)
     print("Train finished")
+
+    if training_job.state == "FAILED":
+        raise "Training job failed!"
 
     prediction_results_dir = '{}/prediction_results/'.format(storage_root)
     print("Start prediction...")
-    estimator.predict_files([storage_train_tiles],
+    prediction_job = estimator.predict_files([storage_train_tiles],
                             output_path=prediction_results_dir,
                             confidence=0.2)
-    print("Task: {}".format(estimator.prediction_job.id))
-    while estimator.prediction_job.is_running():
-        time.sleep(60)
+    print("Task: {}".format(prediction_job.id))
+    while prediction_job.is_running():
+        time.sleep(5)
     print("Predict finished")
+
+    if prediction_job.state == "FAILED":
+        raise "Prediction job failed!"
 
     for path in estimator.prediction_job.metadata["results_files"]:
         File.get(path).download("vineyard/predict-results/")
