7635c643cbce5a20a48582db69ea49a3ffd2ca9d
Damián Silvani <munshkr@gmail.com>
2020-08-03 12:57:48 -0300

    Move file migration scripts as management commands scripts

---
+++ terra/storage/management/commands/import_files.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+
+from projects.models import Project
+from storage.client import Client
+from storage.models import File
+
+
+class Command(BaseCommand):
+    help = 'Creates Files for each blob in user storage (deprecated)'
+
+    def handle(self, *args, **options):
+        for project in Project.objects.all():
+            self.stdout.write(f"Loading files from {project}")
+            client = Client(project)
+            files = client.list_files()
+            for file in files:
+                File.objects.get_or_create(project=project,
+                                           path=file.path,
+                                           defaults={
+                                               'size': file.blob.size,
+                                               'metadata': file.metadata
+                                           })
---
+++ terra/storage/management/commands/migrate_project_files_to_storage.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import tempfile
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from common.utils import gsutilCopy
+from projects.models import File
+from storage.client import Client
+
+
+class Command(BaseCommand):
+    help = 'Migrate project Files to user storage (deprecated)'
+
+    def handle(self, *args, **options):
+        files = File.objects.all()
+        count = files.count()
+        c = 0
+        for file in files:
+            print("Migrando {} de {}...".format(c, count))
+            result = self.migrate_to_storage(file)
+            if result:
+                print("Migracion {} exitosa".format(c))
+            else:
+                print("Migracion {} fallida".format(c))
+            c = c + 1
+
+    def migrate_to_storage(self, file):
+        client = Client(file.project)
+        try:
+            with tempfile.NamedTemporaryFile() as tmpfile:
+                shutil.copyfileobj(file.file, tmpfile)
+                src = tmpfile.name
+                gsutilCopy(
+                    src,
+                    'gs://{bucket}/project_{project_id}/{filename}'.format(
+                        bucket=settings.FILES_BUCKET,
+                        project_id=file.project.pk,
+                        filename=file.name))
+            return True
+        except Exception as e:
+            print(e)
+            return False
