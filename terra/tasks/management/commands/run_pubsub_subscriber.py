b0ef0072a9a9eb9bfd4e28ae3db125b745682c1a
Damián Silvani <munshkr@gmail.com>
2020-08-03 13:18:46 -0300

    Move completed_job_subscriber script as a tasks management command
    Rename to run_pubsub_subscriber

---
+++ terra/tasks/management/commands/run_pubsub_subscriber.py
@@ -0,0 +1,51 @@
+import json
+import time
+from datetime import datetime
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from google.cloud import pubsub_v1
+
+from common.utils import gsutilCopy
+from tasks.models import Task, TaskLogEntry
+
+
+class Command(BaseCommand):
+    help = 'Creates Files for each blob in user storage (deprecated)'
+
+    def handle(self, *args, **options):
+        client = pubsub_v1.SubscriberClient()
+        subscription_path = client.subscription_path(
+            settings.PUBSUB_PROJECT_ID, settings.PUBSUB_JOB_TOPIC_ID)
+
+        def callback(message):
+            self.stdout.write(f'Job log: {message.data}')
+            try:
+                data = json.loads(message.data.decode('utf8'))
+
+                task_id = data.get("task_id")
+                if not task_id:
+                    self.stdout.write(f'Message with no task id: {data}')
+                    return
+
+                task = Task.objects.filter(pk=int(task_id)).first()
+                if task is not None:
+                    TaskLogEntry.objects.create(
+                        task=task,
+                        log=data["payload"],
+                        logged_at=datetime.strptime(data["logged_at"],
+                                                    '%Y-%m-%d %H:%M:%S.%f'),
+                    )
+                else:
+                    self.stdout.write(f'Unknow Task message: {message.data}')
+            except Exception as err:
+                raise err
+            else:
+                message.ack()
+
+        self.stdout.write(f"Subscribe to: {subscription_path}")
+        client.subscribe(subscription_path, callback=callback)
+
+        self.stdout.write("Waiting for completed tasks...")
+        while True:
+            time.sleep(30)
