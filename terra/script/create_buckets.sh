ae9203861d174da681580e811e630348a6114b41
Damián Silvani <munshkr@gmail.com>
2020-08-11 12:34:51 -0300

    Script for creating development buckets

---
+++ terra/script/create_buckets.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+lifecycle_json=/tmp/lifecycle.json
+echo '{
+  "lifecycle": {
+    "rule": [{
+      "action": {"type": "Delete"},
+      "condition": {
+        "age": 7,
+        "isLive": true
+      }
+    }]
+  }
+}' > $lifecycle_json
+
+project=dyma-development
+prefix="${project}-$(whoami)-"
+buckets="tiles files task-artifacts models"
+
+for bucket in $buckets; do
+  bucket="gs://${prefix}${bucket}"
+  ans=0
+  gsutil ls -p $project $bucket || ans=$?
+  if [ $ans -ne 0 ]; then
+    echo "Creating $bucket..."
+    gsutil mb -p $project $bucket
+  fi
+  gsutil lifecycle set $lifecycle_json $bucket
+done
