09d5da208bf34269ddac2cbcdc93108832598257
Damián Silvani <munshkr@gmail.com>
2020-08-12 13:56:11 -0300

    Put Sign up buttom at the AppBar

--- aqua/components/SignupButton.js
+++ aqua/components/SignupButton.js
@@ -1,30 +1,28 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { withTranslation } from "../i18n";
-import { withStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
+import { Link } from "../i18n";
+import { withStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
 
-const styles = theme => ({
+const styles = (theme) => ({
   btn: {
-    position: "fixed",
-    right: 10,
-    bottom: 10,
     margin: theme.spacing(1),
-    zIndex: 1000
-  }
+    zIndex: 1000,
+  },
 });
 
 const SignupButton = ({ t, classes }) => (
-  <a href="/signup" target="_blank">
+  <Link href="/signup">
     <Button className={classes.btn} color="primary" variant="contained">
       {t("btn_signup")}
     </Button>
-  </a>
+  </Link>
 );
 
 SignupButton.propTypes = {
   classes: PropTypes.object.isRequired,
-  t: PropTypes.func.isRequired
+  t: PropTypes.func.isRequired,
 };
 
 export default withTranslation("testdrive")(withStyles(styles)(SignupButton));
