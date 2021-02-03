dc3455f74e612391178df6d326f6905707f2ca57
Damián Silvani <munshkr@gmail.com>
2021-02-03 16:52:11 -0300

    terra: Add estimators_module boolean flag on Projects

--- terra/projects/models.py
+++ terra/projects/models.py
@@ -42,6 +42,8 @@ class Project(models.Model):
                                    max_length=255,
                                    blank=True)
     no_images = models.BooleanField(_("No images"), default=False)
+    estimators_module = models.BooleanField(_("Enable Estimators module"),
+                                            default=True)
 
     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
