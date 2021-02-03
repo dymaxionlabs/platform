54c39b5094ffdbd734acf6ede7c51677cf6f94d6
Damián Silvani <munshkr@gmail.com>
2021-02-03 17:33:47 -0300

    aqua: Get dashboard URL from projects API

--- aqua/components/home/TablesContent.js
+++ aqua/components/home/TablesContent.js
@@ -3,11 +3,12 @@ import { withStyles } from "@material-ui/core/styles";
 
 import { withSnackbar } from "notistack";
 import PropTypes from "prop-types";
+import cookie from "js-cookie";
+import axios from "axios";
 import React from "react";
-import { i18n, withTranslation } from "../../i18n";
+import { withTranslation } from "../../i18n";
 import { buildApiUrl } from "../../utils/api";
 import { logout } from "../../utils/auth";
-import FileDownload from "../../utils/file-download";
 
 const styles = (theme) => ({
   root: {
@@ -22,8 +23,42 @@ const styles = (theme) => ({
 });
 
 class TablesContent extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  async getDashboard() {
+    const projectId = cookie.get("project");
+
+    try {
+      const response = await axios.get(buildApiUrl(`/projects/${projectId}/`), {
+        headers: { Authorization: this.props.token },
+      });
+      console.log(response.data);
+      this.setState({
+        dashboardUrl: response.data.redash_dashboard_url,
+      });
+    } catch (err) {
+      const response = err.response;
+      if (response && response.status === 401) {
+        logout();
+      } else {
+        console.error(response);
+        this.props.enqueueSnackbar("Failed to get dashboard content", {
+          variant: "error",
+        });
+      }
+    }
+  }
+
+  async componentDidMount() {
+    await this.getDashboard();
+    this.setState({ loading: false });
+  }
+
   render() {
     const { t, classes } = this.props;
+    const { dashboardUrl } = this.state;
 
     return (
       <div>
@@ -33,7 +68,7 @@ class TablesContent extends React.Component {
         <Paper className={classes.root}>
           <iframe
             style={{ border: "none", width: "100vw", height: "100vh" }}
-            src="https://redash-demo.dymaxionlabs.com/public/dashboards/KLb0FkID7kVVycpm0OmNCazxiZj4UulX4vnNPKbM?org_slug=default"
+            src={dashboardUrl}
           ></iframe>
         </Paper>
       </div>
