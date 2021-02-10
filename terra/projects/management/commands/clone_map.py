2c5953ccc1899a80c9c41962bf44d407c938c4d7
Damián Silvani <munshkr@gmail.com>
2021-02-10 17:25:35 -0300

    projects: Fix clone map

--- terra/projects/management/commands/clone_map.py
+++ terra/projects/management/commands/clone_map.py
@@ -1,5 +1,7 @@
+from uuid import uuid4
 from django.core.management.base import BaseCommand, CommandError
-from projects.models import Project, Map, Layer
+from projects.models import Project, Map, Layer, MapLayer
+import subprocess
 
 
 class Command(BaseCommand):
@@ -17,20 +19,49 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         project = Project.objects.get(pk=options['project_id'])
-        orig_map = Map.objects.get(pk=options['map_id'])
 
-        new_map = orig_map.clone()
-        new_map.project = project
+        new_map = Map.objects.get(pk=options['map_id'])
         new_map.pk = None
-        new_map.uuid = None
+        new_map.project = project
+        new_map.uuid = uuid4()
         new_map.save()
 
-        for layer in orig_map.layers():
-            new_layer = layer.clone()
-            new_layer.project = project
+        orig_map = Map.objects.get(pk=options['map_id'])
+        map_layers = MapLayer.objects.filter(map=orig_map)
+        for new_map_layer in map_layers:
+            new_layer = new_map_layer.layer
             new_layer.pk = None
-            new_layer.uuid = None
-            new_layer.map = new_map
+            new_layer.project = project
+            orig_layer_uuid = new_layer.uuid
+            new_layer.uuid = uuid4()
             new_layer.save()
-            # TODO Copy tiles fron old layer to new layer directory
-            # ...
+
+            # Copy tiles fron old layer to new layer directory
+            self.copy_layer_tiles(orig_layer_uuid, new_layer.uuid)
+
+            new_map_layer.pk = None
+            new_map_layer.map = new_map
+            new_map_layer.layer = new_layer
+            new_map_layer.save()
+
+    def copy_layer_tiles(self, src_uuid, dst_uuid):
+        src = Layer.objects.get(uuid=src_uuid)
+        dst = Layer.objects.get(uuid=dst_uuid)
+        src_url = src.tiles_bucket_url()
+        dst_url = dst.tiles_bucket_url()
+        if src.layer_type == Layer.RASTER:
+            self.run_command(
+                f"gsutil -m cp -a public-read -r {src_url}/* {dst_url}/")
+        elif src.layer_type == Layer.VECTOR:
+            self.run_command(
+                f"gsutil -m -h \"Content-Type: application/octet-stream\" -h \"Content-Encoding: gzip\" cp -a public-read -r {src_url}/* {dst_url}/"
+            )
+            self.run_command(
+                f"gsutil -m -h \"Content-Type: application/json\" cp -a public-read -r {src_url}/metadata.json {dst_url}/"
+            )
+        else:
+            raise RuntimeError(f"Unknown layer type! {src.layer_type}")
+
+    def run_command(self, cmd):
+        print(cmd)
+        subprocess.run(cmd, shell=True, check=True)
