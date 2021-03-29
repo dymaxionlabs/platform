abfe19f3a931c6848dba3c303873d046e3c09452
Damián Silvani <munshkr@gmail.com>
2021-03-29 12:20:02 -0300

    terra: Fix migration

--- terra/projects/migrations/0055_add_dashboard_from_redash_dashboard_url.py
+++ terra/projects/migrations/0055_add_dashboard_from_redash_dashboard_url.py
@@ -5,6 +5,7 @@ from django.db import migrations
 
 def add_dashboard_from_redash_dashboard_url(apps, schema_editor):
     Project = apps.get_model("projects", "project")
+    Dashboard = apps.get_model("projects", "dashboard")
     for project in Project.objects.all():
         if project.redash_dashboard_url:
             Dashboard.objects.get_or_create(
