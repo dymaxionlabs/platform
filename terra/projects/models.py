d663751ea80b2799c49d7de6b7d44a506cd28652
Damián Silvani <munshkr@gmail.com>
2021-03-29 12:15:39 -0300

    terra: Remove redash_dashboard_url field on Project

--- terra/projects/models.py
+++ terra/projects/models.py
@@ -44,7 +44,6 @@ class Project(models.Model):
     dashboards_module = models.BooleanField(
         _("Enable Dashboards module"), default=False
     )
-    redash_dashboard_url = models.URLField(_("Redash Dashboard URL"), blank=True)
 
     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
