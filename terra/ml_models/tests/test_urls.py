0d54518b6db208640b5efd2a3b20e92ee568b694
Lucas <lucasmiguelmac@gmail.com>
2022-07-18 18:30:26 +0000

    Test models urls

--- terra/ml_models/tests/test_urls.py
+++ terra/ml_models/tests/test_urls.py
@@ -4,6 +4,9 @@ from django.urls import resolve, reverse
 
 class TestMLModelViewSetURLs:
     @pytest.mark.parametrize('incoming_path, viewname',[
+        ('/users/lucas/models/',                                    'UserMLModelViewSet'),
+        ('/users/lucas/models/test_model/',                         'UserMLModelViewSet'),
+        ('/users/lucas/models/test_model/versions/',                'MLModelVersionViewSet'),
         ('/users/lucas/models/test_model/versions/latest/',         'MLModelVersionViewSet'),
         ('/users/lucas/models/test_model/versions/latest/predict/', 'MLModelVersionViewSet'),
     ])
