457f0c93a148cc3e6b9c8629d49d9da88f739aa1
Lucas <lucasmiguelmac@gmail.com>
2022-07-19 19:44:23 +0000

    Add resolvers to urls tests

--- terra/ml_models/tests/test_urls.py
+++ terra/ml_models/tests/test_urls.py
@@ -2,14 +2,21 @@ import pytest
 from django.urls import resolve, reverse
 
 
-class TestMLModelViewSetURLs:
-    @pytest.mark.parametrize('incoming_path, viewname',[
-        ('/users/lucas/models/',                                    'UserMLModelViewSet'),
-        ('/users/lucas/models/test_model/',                         'UserMLModelViewSet'),
-        ('/users/lucas/models/test_model/versions/',                'MLModelVersionViewSet'),
-        ('/users/lucas/models/test_model/versions/latest/',         'MLModelVersionViewSet'),
-        ('/users/lucas/models/test_model/versions/latest/predict/', 'MLModelVersionViewSet'),
-    ])
-    def test_url_name(self, incoming_path, viewname):
-        resolver = resolve(incoming_path)
+class TestMLModelURLs:
+    
+    @pytest.mark.parametrize(
+        'url_path, viewname, url_kwargs, url_name',
+        (
+            ('/users/test_user/models/',                                            'UserMLModelViewSet',       {"user_username": "test_user"},                             'models-list'),
+            ('/users/test_user/models/test_model/',                                 'UserMLModelViewSet',       {"user_username": "test_user", "name": "test_model"},                             'models-detail'),
+            ('/users/test_user/models/test_model/versions/',                        'MLModelVersionViewSet',    {"user_username": "test_user", "model_name": "test_model"}, 'versions-list'),
+            ('/users/test_user/models/test_model/versions/test_version/',           'MLModelVersionViewSet',    {"user_username": "test_user", "model_name": "test_model", "name": "test_version"}, 'versions-detail'),
+            ('/users/test_user/models/test_model/versions/test_version/predict/',   'MLModelVersionViewSet',    {"user_username": "test_user", "model_name": "test_model", "name": "test_version"}, 'versions-predict'),
+        )
+    )
+    def test_urls(self, url_path, viewname, url_kwargs, url_name):
+        resolver = resolve(url_path)
         assert resolver.func.__name__ == viewname
+
+        url = reverse(url_name, kwargs=url_kwargs)
+        assert url == url_path
