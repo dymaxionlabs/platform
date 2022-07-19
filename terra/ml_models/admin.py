e796961d5d8af428b81cefbb4a892946fa6546cf
Damián Silvani <munshkr@gmail.com>
2022-07-19 19:45:41 -0300

    feat: Register an admin for MLModel with MLModelVersion as inlines

--- terra/ml_models/admin.py
+++ terra/ml_models/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MLModel, MLModelVersion
+
+
+class MLModelVersionInline(admin.TabularInline):
+    model = MLModelVersion
+
+
+class MLModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "owner",
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+        "latest_version"
+    )
+    inlines = [MLModelVersionInline]
+
+    def latest_version(self, obj):
+        last_version = obj.mlmodelversion_set.last()
+        return last_version.name if last_version else '-'
+
+
+admin.site.register(MLModel, MLModelAdmin)
