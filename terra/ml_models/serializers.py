f0a8238490de2d75feed5804edab166151904553
Damián Silvani <munshkr@gmail.com>
2022-07-20 08:15:39 -0300

    fix: remove `fields` on `MLModelSerializer`

--- terra/ml_models/serializers.py
+++ terra/ml_models/serializers.py
@@ -3,14 +3,16 @@ from ml_models.models import MLModel, MLModelVersion
 
 
 class BaseMeta:
-    fields = '__all__'
-    read_only_fields = ('created_at', 'updated_at')
+    read_only_fields = ("created_at", "updated_at")
+
 
 class MLModelSerializer(serializers.ModelSerializer):
     class Meta(BaseMeta):
         model = MLModel
-        exclude = ('lf_project_id',)
+        exclude = ("lf_project_id",)
+
 
 class MLModelVersionSerializer(serializers.ModelSerializer):
     class Meta(BaseMeta):
         model = MLModelVersion
+        fields = "__all__"
