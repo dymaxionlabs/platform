a84cc358964adfe74e4dec2c1677ee4b3668c5cc
Lucas <lucasmiguelmac@gmail.com>
2022-07-25 14:45:44 +0000

    Rearrange serializers

--- terra/ml_models/serializers.py
+++ terra/ml_models/serializers.py
@@ -5,18 +5,6 @@ from ml_models.models import MLModel, MLModelVersion
 class BaseMeta:
     read_only_fields = ("created_at", "updated_at")
 
-
-class MLModelVersionSerializer(serializers.ModelSerializer):
-    model = serializers.SerializerMethodField()
-
-    class Meta(BaseMeta):
-        model = MLModelVersion
-        exclude = ("id",)
-
-    def get_model(self, obj):
-        return str(obj.model)
-
-
 class MLModelSerializer(serializers.ModelSerializer):
     owner = serializers.SlugRelatedField(read_only=True, slug_field="username")
     latest_version = serializers.SerializerMethodField()
@@ -31,3 +19,14 @@ class MLModelSerializer(serializers.ModelSerializer):
     def get_latest_version(self, obj):
         version = obj.latest_version
         return version.name if version else None
+
+
+class MLModelVersionSerializer(serializers.ModelSerializer):
+    model = serializers.SerializerMethodField()
+
+    class Meta(BaseMeta):
+        model = MLModelVersion
+        exclude = ("id",)
+
+    def get_model(self, obj):
+        return str(obj.model)
