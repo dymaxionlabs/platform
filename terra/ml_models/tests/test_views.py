c7fc2ae0df197dfd7ea903f22410e9065a66881c
Lucas <lucasmiguelmac@gmail.com>
2022-07-25 18:24:07 +0000

    Test viewsets for MLModel

--- terra/ml_models/tests/test_views.py
+++ terra/ml_models/tests/test_views.py
@@ -6,43 +6,34 @@ from django.contrib.auth import get_user_model
 from django_mock_queries.mocks import MockSet
 from model_bakery import baker
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import AnonymousUser
 
 from ..serializers import MLModelSerializer
 from ..views import MLModelViewSet
 from ..models import MLModel
-from .fixtures import mock_authentication
 
-ml_model_fields = [
-    field.name for field in MLModel._meta.get_fields()
-]
 
+ml_model_fields = [field.name for field in MLModel._meta.get_fields()]
 prepare_ml_model = lambda self=None: baker.prepare(MLModel)
+ml_model_1 = prepare_ml_model()
 test_user = baker.prepare(get_user_model())
 
+
 class TestAllMLModelViewset:
-    ml_model_1 = prepare_ml_model()
-    valid_data_dict = {
-        k: v for (k, v) in ml_model_1.__dict__.items() if k in ml_model_fields
-    }
-    
-    def test_list(self, mocker, rf, mock_authentication):
-        
+    def test_list(self, mocker, rf):
+
         # Arrange
         qs = MockSet(
-            self.ml_model_1,
+            ml_model_1,
             prepare_ml_model(),
             prepare_ml_model(),
         )
-        url = reverse('mlmodel-list', kwargs=None)
+        url = reverse("mlmodel-list", kwargs=None)
         request = rf.get(url)
-        view = MLModelViewSet.as_view(
-            {'get': 'list'}
-        )
+        view = MLModelViewSet.as_view({"get": "list"})
 
         # Mock
-        mocker.patch.object(
-            MLModelViewSet, 'get_queryset', return_value=qs
-        )
+        mocker.patch.object(MLModelViewSet, "get_queryset", return_value=qs)
 
         # Act
         response = view(request).render()
@@ -51,4 +42,57 @@ class TestAllMLModelViewset:
         assert response.status_code == 200
         assert json.loads(response.content)["count"] == 3
 
-    
+
+class TestMLModelViewset:
+
+    valid_data_dict = {
+        k: v for (k, v) in ml_model_1.__dict__.items() if k in ml_model_fields
+    }
+
+    def test_list(self, mocker, rf):
+
+        # Arrange
+        qs = MockSet(
+            ml_model_1,
+            prepare_ml_model(),
+            prepare_ml_model(),
+        )
+        url = reverse("models-list", kwargs={"user_username": test_user.get_username()})
+        request = rf.get(url)
+        view = MLModelViewSet.as_view({"get": "list"})
+
+        # Mock
+        mocker.patch.object(MLModelViewSet, "get_queryset", return_value=qs)
+
+        # Act
+        response = view(request).render()
+
+        # Assert
+        assert response.status_code == 200
+        assert json.loads(response.content)["count"] == 3
+
+    def test_retrieve(self, mocker, rf):
+
+        # Arrange
+        endpoint_kwargs = {"user_username": test_user.get_username(), "name": ml_model_1.name}
+        url = reverse(
+            "models-detail",
+            kwargs=endpoint_kwargs,
+        )
+        request = rf.get(url)
+        view = MLModelViewSet.as_view({"get": "retrieve"})
+
+        # Mock
+        mocker.patch.object(
+            MLModelViewSet, "get_queryset", return_value=MockSet(ml_model_1)
+        )
+        mocker.patch.object(
+            MLModelViewSet, "get_queryset", return_value=MockSet(ml_model_1)
+        )
+
+        # Act
+        response = view(request, **endpoint_kwargs).render()
+
+        # Assert
+        assert response.status_code == 200
+
