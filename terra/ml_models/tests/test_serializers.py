090abd9cd12efc0a0e7a9b453b90b69b7582f50f
Lucas <lucasmiguelmac@gmail.com>
2022-07-25 23:37:49 +0000

    refactor test_deserialize_fields examples

--- terra/ml_models/tests/test_serializers.py
+++ terra/ml_models/tests/test_serializers.py
@@ -25,21 +25,23 @@ class TestMLModelSerializer:
         assert serializer.is_valid()
         assert serializer.errors == {}
 
-
     ml_model_name_max_chars = 134
 
-    @pytest.mark.parametrize("wrong_field", (
-        {"name": "a" * (ml_model_name_max_chars + 1)},
-        {"tags": "tag outside of array"},
-        {"tags": ["--------wrong length tag--------"]},
-        {"version": "wronglengthversion"},
-        {"is_public": 1},
-        {"is_public": "Nope"},
-    ))
+    @pytest.mark.parametrize(
+        "wrong_field",
+        (
+            {"name": "a" * (ml_model_name_max_chars + 1)},
+            {"tags": "tag-outside-of-array"},
+            {"repo_url": "wron_format_url"},
+            {"tags": ["--------wrong length tag--------"]},
+            {"is_public": "Nope"},
+        ),
+    )
     def test_deserialize_fails(self, wrong_field: dict):
-        ml_model_fields = [field.name for field in MLModel._meta.get_fields()]
         invalid_serialized_data = {
-            k: v for (k, v) in self.ml_model.__dict__.items() if k in ml_model_fields and k != "id"
+            k: v
+            for (k, v) in self.ml_model.__dict__.items()
+            if k not in MLModelSerializer.Meta.exclude
         } | wrong_field
 
         serializer = MLModelSerializer(data=invalid_serialized_data)
