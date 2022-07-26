3386c5edb45e5b3b17268d3c5180df7f71e3858b
Lucas <lucasmiguelmac@gmail.com>
2022-07-26 16:42:02 +0000

    Layout permissions tests

---
+++ terra/ml_models/tests/test_permissions.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+class TestIsOwnerOrReadOnly:
+
+    def test_safe_method(self):
+        pass
+
+    def test_user_is_owner_unsafe_method(self):
+        pass
+
+    def test_user_not_owner_unsafe_method(self):
+        pass
+
+class TestIsModelPublic:
+
+    def test_is_public(self):
+        pass
+
+    def test_is_not_public(self):
+        pass
+
+
+class TestIsModelVersionPublic:
+
+    def test_is_public(self):
+        pass
+
+    def test_is_not_public(self):
+        pass
