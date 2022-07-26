39bb04c65e5e7756ecfb09bd8eb5bfe13be4fe62
Lucas <lucasmiguelmac@gmail.com>
2022-07-26 18:09:52 +0000

    Test Permissions: TestIsOwnerOrReadOnly

--- terra/ml_models/tests/test_permissions.py
+++ terra/ml_models/tests/test_permissions.py
@@ -1,19 +1,52 @@
 import pytest
+from model_bakery import baker
+from django.contrib.auth import get_user_model
+
+from ml_models.permissions import IsOwnerOrReadOnly
 
 
 class TestIsOwnerOrReadOnly:
+    def test_safe_method(self, rf):
 
-    def test_safe_method(self):
-        pass
+        # Arrange
+        request = rf.get("/")
 
-    def test_user_is_owner_unsafe_method(self):
-        pass
+        # Act
+        perm_check = IsOwnerOrReadOnly()
 
-    def test_user_not_owner_unsafe_method(self):
-        pass
+        # Assert
+        assert perm_check.has_object_permission(request, None, None)
+
+    def test_user_is_owner_unsafe_method(self, rf):
+
+        # Arrange
+        request = rf.post("/")
+        request_user = baker.prepare(get_user_model())
+        request.user = request_user
+        obj = type("TestObj", (object,), {"owner": request_user})
+
+        # Act
+        perm_check = IsOwnerOrReadOnly()
+
+        # Assert
+        assert perm_check.has_object_permission(request, None, obj)
+
+    def test_user_not_owner_unsafe_method(self, rf):
+
+        # Arrange
+        request = rf.post("/")
+        request_user, obj_owner = baker.prepare(get_user_model(), _quantity=2)
+        request.user = request_user
+        obj = type("TestObj", (object,), {"owner": 1})
+
+        # Act
+        perm_check = IsOwnerOrReadOnly()
+
+        # Assert
+        assert not perm_check.has_object_permission(request, None, obj)
+
 
 class TestIsModelPublic:
-
     def test_is_public(self):
         pass
 
@@ -22,7 +55,6 @@ class TestIsModelPublic:
 
 
 class TestIsModelVersionPublic:
-
     def test_is_public(self):
         pass
 
